467d0dc33b6603ebe77319aff308cc840df4d725
noknight12 <karlonasa12@gmail.com>
2025-12-16 18:56:06 -0800

    blue square

--- renderer/src/App.jsx
+++ renderer/src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-function App() {
+import Home from './Home'
+const App = () => {
   const [count, setCount] = useState(0)
 
   return (
@@ -28,6 +28,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <Home/>
     </>
   )
 }
---
+++ renderer/src/Home.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import React from "react";
+
+
+const Home = () => {
+
+
+  return(<>
+  <div style={{ color: "white", fontSize: "20px", padding: "10px", height: "50px", width: "50px", backgroundColor: "blue"}}>
+    Hello
+    </div>
+    </>
+)
+  
+};
+
+export default Home;
