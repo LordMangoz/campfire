7530a29a39f5c725a1a2337b5395d87b97bd774c
umanga <umangabajgai@gmail.com>
2025-12-16 16:34:41 -0800

    third commit set up complete

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ const {BrowserWindow, app} = require('electron');
 
 console.log('Hello from Electron 👋');
 
+const createWindow = () => {
+    const window = new BrowserWindow({
+        width:800,
+        height:600
+    })
+}
+
+app.whenReady().then(() => {
+    createWindow();
+});
